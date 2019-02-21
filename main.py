2bc4057cd3cbf6e3c2dae873457fab6045ef69bd
Gagan Devagiri <gagandevagiri@gmail.com>
2019-02-21 15:55:49 +0000

    Updated deadoralive function, fixed some bugs

--- main.py
+++ main.py
@@ -13,28 +13,50 @@ Subject: Hello peasents!
 Your cow died lol
 ."""
 
-
-
 #### EMAIL CONFIG ####
 
 
 
 def DeadorAlive():
     # add functions instead of the actual value.
-    currentTemp = 10
-    currentHeartRate = 10
-    currentPace = 0
+    global currentTemp = 10
+    global currentHeartRate = 10
+    global currentPace = 0
     if (currentTemp <= 15 and currentHeartRate <= 10 and currentPace < 10) :
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message)
-        return "cow died"
+        return False
     else:
+        return True
 
-        return "cow didn't die"
-alive = DeadorAlive() # Dead or alive
-print(alive)
+def Fever():
+    global varFever = currentTemp + currentHeartRate
+    if (varFever >= 130):
+        return False
+    else:
+        return True
+
+def EastCoastFever():
+    if (Fever):
+        RelativeTemp = #RelativeTemp()
+        if RelativeTemp > 5:
+            
+            message = f"""
+            Subject: East Coast Fever alert!
+            
+            
+            Our system has diagnosed cow number {num} with possible """
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, message)
+                return False
+            else:
+                return True
+
+alive = DeadorAlive()
 
 # def main() 
 # if state:
@@ -43,4 +65,4 @@ print(alive)
 #     # then declare dead 
 #     return "Dead" # notify farmer that the cow is dead.
 # #else:
-    
+num = 2
