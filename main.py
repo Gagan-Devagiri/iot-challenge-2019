f962d4549ea31d9c1cf34be89ccd82bd59fa9bf6
Gagan Devagiri <gagandevagiri@gmail.com>
2019-02-22 12:12:05 +0000

    Made the code more modular

--- main.py
+++ main.py
@@ -7,13 +7,8 @@ smtp_server = "smtp.gmail.com"
 sender_email = "eastcoastdetector@gmail.com"  # Enter your address
 receiver_email = "gagandevagiri@gmail.com"  # Enter receiver address
 password = input("Type the id and press enter: ")
-message = """\
-Subject: Your cow died lol!
 
-lmao
-."""
-
-num = 123
+num = 420
 
 #### EMAIL CONFIG ####
 
@@ -26,6 +21,11 @@ def DeadorAlive():
     global currentHeartRate
     currentHeartRate = 70
     if (currentTemp <= 15 and currentHeartRate <= 10) :
+        message = """\
+Subject: Your cow died lol!
+
+lmao
+."""
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
@@ -38,35 +38,49 @@ def Fever():
     global varFever
     varFever = currentTemp + currentHeartRate
     if (varFever >= 100):
-        return False
-    else:
         return True
+    else:
+        return False
+
+def sendEmail():
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        return True
+
 
 def EastCoastFever(Fever):
     if (Fever):
         RelativeTemp = 10#RelativeTemp()
         if RelativeTemp > 5:
+            global message
             message = f"""
             Subject: East Coast Fever alert!
             
-            
+
             Our system has diagnosed cow number {num} possibility of East coast fever.
             Please take appropiate action. """
-            return False
+            x = sendEmail()
+            return x
         else:
+            global message
             message = f"""
             Subject: East Coast Fever alert!
 
 
             Our system has diagnosed cow number {num} possibility of fever & a low possibility of east coast fever.
             We reccomend that you take appropiate action immediately. """
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-            return False
+            x = sendEmail()
+            return x
     else:
-        return False, "Cow's healthy"
+        global message
+        message = f"""
+        Subject: Daily update! (No action required)
+
+
+        Your cow's healthy.
+        """
 
 
 alive = DeadorAlive()
@@ -74,10 +88,13 @@ if (alive):
     fever = Fever()
 else:
     fever = False
-EastCoastFever(fever)
+ECF = EastCoastFever(fever)
 if (not fever):
     print("Cow's healthy")
 
+def main():
+    if state:
+        heart rate 
 # def main() 
 # if state:
 #     # heart rate not less than 20.
@@ -85,4 +102,3 @@ if (not fever):
 #     # then declare dead 
 #     return "Dead" # notify farmer that the cow is dead.
 # #else:
-num = 2
